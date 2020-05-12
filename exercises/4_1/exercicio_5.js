3ad6f99ffd3312281583b3c203fb68251461242a
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-12 18:39:44 -0300

    Exercicio 5

---
+++ exercises/4_1/exercicio_5.js
@@ -0,0 +1,9 @@
+let a = 45 ; let b = 90 ; let c = 45;
+
+if ( a + b + c == 180) {
+  console.log(true);
+} else if ( a + b + c != 180) {
+  console.log(false);
+} else {
+  console.error("Valor inválido!");
+}
