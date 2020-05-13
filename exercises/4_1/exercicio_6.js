c745dcc7486bc5b919577442d856b8ca1bae5cb7
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-13 18:40:35 -0300

    Exercicios de 1 a 3

---
+++ exercises/4_1/exercicio_6.js
@@ -0,0 +1,18 @@
+let peca = "rainha";
+
+if (peca == "rainha"){
+  console.log("Vertical, horizontal e diagonalmente")
+
+} else if (peca == "bispo"){
+  console.log("Diagonalmente")
+
+} else if (peca == "rei") {
+  console.log("Qualquer casa adjacente")
+
+} else if (peca == "cavalo") {
+  console.log("Vertical ou horizontal")
+
+} else if (peca == torre) {
+  console.log("Vertical, horizontal")
+
+}
